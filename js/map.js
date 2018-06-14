b3a03d3a730e653e520bb9371c45872cb1a5d78a
Aleksandr Dmitriev <dmitriev@gorodkolomna.ru>
2018-06-14 21:16:13 +0300

    Исправлены замечания линтера

--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
 var NUMBER_OF_OBJECTS = 8;
-var TITLE_ARR = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
+var TITLE_ARR = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var TYPE_ARR = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN_ARR = ['12:00', '13:00', '14:00'];
 var CHECKOUT_ARR = ['12:00', '13:00', '14:00'];
-var FEATURES_ARR = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var PHOTOS_ARR = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
+var FEATURES_ARR = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS_ARR = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var X_MIN = 300;
 var X_MAX = 900;
 var Y_MIN = 130;
@@ -25,22 +25,22 @@ function shuffleArr(array) {
   for (var i = array.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
     var temp = array [i];
-    array [i]= array[j];
+    array [i] = array[j];
     array[j] = temp;
-  };
+  }
   return array;
-};
+}
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max +1 - min)) + min;
-};
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
 
 function getRndItems(n) {
-  return Math.floor(Math.random() * (n + 1))
-};
+  return Math.floor(Math.random() * (n + 1));
+}
 
-var createMockData = function (mockDataArray) {
-  for (var i = 1; i <= NUMBER_OF_OBJECTS; i++){
+var createMockData = function (array) {
+  for (var i = 1; i <= NUMBER_OF_OBJECTS; i++) {
     var x = getRandomInt(X_MIN, X_MAX);
     var y = getRandomInt(Y_MIN, Y_MAX);
     var featuresNewArr = shuffleArr(FEATURES_ARR);
@@ -52,7 +52,7 @@ var createMockData = function (mockDataArray) {
 
       offer: {
         title: getRndArrayItem(TITLE_ARR),
-        address: x +', ' + y,
+        address: x + ', ' + y,
         price: getRandomInt(PRICE_MIN, PRICE_MAX),
         type: getRndArrayItem(TYPE_ARR),
         rooms: getRandomInt(ROOMS_MIN, ROOMS_MAX),
@@ -70,10 +70,7 @@ var createMockData = function (mockDataArray) {
       }
     };
     mockDataArray[i] = advert;
-  };
-  return mockDataArray;
+  }
+  return array;
 };
 createMockData(mockDataArray);
-
-
-
