9cef599958139c22d076b712fc8f6735b85e8369
Aleksandr Dmitriev <dmitriev@gorodkolomna.ru>
2018-06-17 15:59:40 +0300

    Создан DOM-элемент объявления, заполнен данными из объекта и вставлен в блок .map

--- js/map.js
+++ js/map.js
@@ -89,8 +89,10 @@ var createPins = function (dataArray) {
   .querySelector('.map__pin');
 
   var fragment = document.createDocumentFragment();
+
   for (var i = 1; i < dataArray.length; i++) {
     var pinElement = pinTemplate.cloneNode(true);
+
     pinElement.setAttribute('style', 'left: ' + (dataArray[i].location.x - PIN_WIDTH / 2) + 'px; top: ' + (dataArray[i].location.y - PIN_HEIGHT) + 'px;');
     var pinImage = pinElement.firstElementChild;
     pinImage.src = dataArray[i].author.avatar;
@@ -101,3 +103,67 @@ var createPins = function (dataArray) {
 };
 
 createPins(createMockData());
+
+var getHousingType = function (arrayItem) {
+  if (arrayItem == 'palace') {
+
+    return 'Дворец';
+
+  } else if (arrayItem == 'flat') {
+
+    return 'Квартира';
+
+  } else if (arrayItem == 'bungalo') {
+
+    return 'Бунгало';
+
+  } else {
+
+    return 'Дом';
+  }
+};
+
+var createAdvert = function (dataArray) {
+  var advertParentElement = document.querySelector('.map');
+  var advertNextElement = document.querySelector('.map__filters-container');
+  var advertTemplate = document.querySelector('template')
+    .content.querySelector('.map__card');
+
+  var advertElement = advertTemplate.cloneNode(true);
+
+  advertElement.querySelector('.popup__avatar').src = dataArray[1].author.avatar;
+  advertElement.querySelector('.popup__title').textContent = dataArray[1].offer.title;
+  advertElement.querySelector('.popup__text--address').textContent = dataArray[1].offer.address;
+  advertElement.querySelector('.popup__text--price').textContent = dataArray[1].offer.price + '₽/ночь';
+  advertElement.querySelector('.popup__type').textContent = getHousingType(dataArray[1].offer.type);
+  advertElement.querySelector('.popup__text--capacity').textContent = dataArray[1].offer.rooms + ' комнаты для ' + dataArray[1].offer.guests + ' гостей';
+  advertElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + dataArray[1].offer.checkin + ', выезд до ' + dataArray[1].offer.checkout;
+  advertElement.querySelector('.popup__features');
+
+  var featuresElement = advertElement.querySelector('.popup__features');
+  while (featuresElement.firstChild) {
+    featuresElement.removeChild(featuresElement.firstChild);
+  }
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < dataArray[1].offer.features.length; i++) {
+    var newFeatureElement = document.createElement('li');
+    newFeatureElement.className = 'popup__feature popup__feature--' + dataArray[1].offer.features[i];
+    fragment.appendChild(newFeatureElement);
+  }
+  featuresElement.appendChild(fragment);
+
+  advertElement.querySelector('.popup__description').textContent = dataArray[1].offer.description;
+
+  var photosElement = advertElement.querySelector('.popup__photos');
+  for (var i = 0; i < dataArray[1].offer.photos.length; i++) {
+    var newPhotoElement = photosElement.querySelector('.popup__photo').cloneNode(true);
+    newPhotoElement.src = dataArray[1].offer.photos[i];
+    photosElement.appendChild(newPhotoElement);
+  }
+  photosElement.removeChild(photosElement.querySelector('.popup__photo'));
+
+  advertParentElement.insertBefore(advertElement, advertNextElement);
+};
+
+createAdvert(createMockData());
