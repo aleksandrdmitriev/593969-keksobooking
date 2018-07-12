e3298c401bdec49c91592b533dfa54e7839b0c36
Aleksandr Dmitriev <dmitriev@gorodkolomna.ru>
2018-07-12 23:11:25 +0300

    Сделана загрузка фото в объявление

--- js/photo-load.js
+++ js/photo-load.js
@@ -1,40 +1,42 @@
 'use strict';
 
 (function () {
+
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var fileChooser = document.querySelector('.ad-form__upload input[type=file]');
   var photoLayout = document.querySelector('.ad-form__photo');
-  var photoTemplate = document.querySelector('template')
-    .content
-    .querySelector('.ad-form__img');
+  var photoTemplate = document.querySelector('template').content.querySelector('.ad-form__img');
 
   fileChooser.addEventListener('change', function () {
-    debugger;
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < fileChooser.files.length; i++) {
+    for (var i = 0; i < fileChooser.files.length; i++)(function (i) {
       var photoElement = photoTemplate.cloneNode(true);
 
       var file = fileChooser.files[i];
       var fileName = file.name.toLowerCase();
 
-      var matches = FILE_TYPES.some(function (it) {
+      var getMatch = function (it) {
         return fileName.endsWith(it);
-      });
+      };
+
+      var matches = FILE_TYPES.some(getMatch);
 
       if (matches) {
+
         var reader = new FileReader();
 
-        reader.addEventListener('load', function () {
+        var renderImg = function () {
           photoElement.src = reader.result;
-
-          fragment.appendChild(photoElement);
-
-        });
+          photoLayout.appendChild(photoElement);
+        };
+        reader.addEventListener('load', renderImg);
 
         reader.readAsDataURL(file);
+
       }
-    }
-    photoLayout.appendChild(fragment);
+
+    })(i);
 
   });
+
 })();
+
